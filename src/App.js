501f18b6092e1a8d3995f5b401654e1243c31ec6
Gio Logist <gio@giologist.com>
2020-02-05 13:36:55 -0800

    Step 5) (*optional*) Environment variables in your React App

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>{process.env.REACT_APP_NAME}</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
